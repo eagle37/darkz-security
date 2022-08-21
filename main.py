2b1d9fa6c563cf977f63f98f4d50102d97ae8ed7
eagle37 <84372934+eagle37@users.noreply.github.com>
2022-08-21 16:50:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 os.system("pip install git+https://github.com/eagle37/Discord-Games && pip install tasksio && pip install httpx")
 os.system("pip install discord.py[speed] && pip install jishaku && pip install psutil && pip install pynacl")
